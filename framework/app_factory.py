f640963d83eed9de9aaf137cf09f8c3d4f456e15
Yonghao Lin <80102852+TheSkyRS@users.noreply.github.com>
2025-11-16 13:48:27 -0500

    update version information (descirption)

--- framework/app_factory.py
+++ framework/app_factory.py
@@ -6,8 +6,8 @@ from resources import api
 def create_app() -> FastAPI:
     app =  FastAPI(
         title="Donor Registry Service",
-        description="Microservice 1 (Sprint 1) — Donor, Organ, Consent",
-        version="0.1.0",
+        description="Microservice 1 (Sprint 2) — Donor, Organ, Consent",
+        version="0.2.0",
     )
     app.include_router(api)
 
